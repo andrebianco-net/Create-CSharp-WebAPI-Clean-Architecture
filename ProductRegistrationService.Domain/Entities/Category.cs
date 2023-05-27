145f29207d806a93602b1cc7098dc643aaf113b9
andrebianco-net <abianco.allegro@gmail.com>
2023-05-27 10:10:57 -0300

    Getting rich the entities avoiding direct access and allowing the objects creation via constructor.

--- ProductRegistrationService.Domain/Entities/Category.cs
+++ ProductRegistrationService.Domain/Entities/Category.cs
@@ -1,7 +1,18 @@
-public class Category
+public sealed class Category
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+
+    public Category(string name)
+    {
+        Name = name;
+    }
+
+    public Category(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
 
     public ICollection<Product> Products { get; set; }
 }
