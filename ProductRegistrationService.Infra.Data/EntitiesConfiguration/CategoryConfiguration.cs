0c1a124c38cbed3a429e39f07278c92ceeee297a
andrebianco-net <abianco.allegro@gmail.com>
2023-05-27 17:58:39 -0300

    Adding configuration files for Category and Product

---
+++ ProductRegistrationService.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductRegistrationService.Domain.Entities;
+
+namespace ProductRegistrationService.Infra.Data.Context.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+        }
+    }
+}
