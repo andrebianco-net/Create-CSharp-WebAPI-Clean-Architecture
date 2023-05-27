b774516dd434760494841d6997618c00f6289d55
andrebianco-net <abianco.allegro@gmail.com>
2023-05-27 15:12:19 -0300

    Adding Unit Test for Category and Product

--- ProductRegistrationService.Domain.Tests/CategoryUnitTest1.cs
+++ ProductRegistrationService.Domain.Tests/CategoryUnitTest1.cs
@@ -1,10 +1,52 @@
-namespace ProductRegistrationService.Domain.Tests;
+using Xunit;
+using FluentAssertions;
+using ProductRegistrationService.Domain.Entities;
 
-public class UnitTest1
+namespace ProductRegistrationService.Domain.Tests
 {
-    [Fact]
-    public void Test1()
+    public class CategoryUnitTest1
     {
+        [Fact]
+        public void CreateCategory_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Category(1, "Category Name ");
+            action.Should()
+                    .NotThrow<ProductRegistrationService.Domain.Validation.DomainExceptionValidation>();
+        }
 
+        [Fact]
+        public void CreateCategory_NegativeIdValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Category(-1, "Category Name ");
+            action.Should()
+                    .Throw<ProductRegistrationService.Domain.Validation.DomainExceptionValidation>()
+                    .WithMessage("Invalid Id value.");
+        }
+
+        [Fact]
+        public void CreateCategory_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Category(1, "Ca");
+            action.Should()
+                    .Throw<ProductRegistrationService.Domain.Validation.DomainExceptionValidation>()
+                    .WithMessage("Invalid name, too short, minimum 3 characters");
+        }
+
+        [Fact]
+        public void CreateCategory_MissingNameValue_DomainExceptionRequiredName()
+        {
+            Action action = () => new Category(1, "");
+            action.Should()
+                    .Throw<ProductRegistrationService.Domain.Validation.DomainExceptionValidation>()
+                    .WithMessage("Invalid name.Name is required");
+        }
+
+        [Fact]
+        public void CreateCategory_WithNullNameValue_DomainExceptionInvalidName()
+        {
+            Action action = () => new Category(1, null);
+            action.Should()
+                    .Throw<ProductRegistrationService.Domain.Validation.DomainExceptionValidation>();
+        }
     }
 }
