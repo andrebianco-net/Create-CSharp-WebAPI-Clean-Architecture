7b06f986f166678676ed217adc564e1e3005d265
andrebianco-net <abianco.allegro@gmail.com>
2023-05-28 15:44:58 -0300

    Adding DTOs Category and Product, as also their auto mapping with domain

---
+++ ProductRegistrationService.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using ProductRegistrationService.Application.DTOs;
+using ProductRegistrationService.Domain.Entities;
+
+namespace ProductRegistrationService.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
