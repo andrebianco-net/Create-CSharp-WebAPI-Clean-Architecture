6c40ded7561865f4e2420006533dcd51e4311b84
andrebianco-net <abianco.allegro@gmail.com>
2023-05-27 22:56:45 -0300

    Fixing parameters wrong

--- ProductRegistrationService.Domain/Interfaces/IProductRepository.cs
+++ ProductRegistrationService.Domain/Interfaces/IProductRepository.cs
@@ -7,8 +7,8 @@ namespace ProductRegistrationService.Domain.interfaces
         Task<IEnumerable<Product>> GetProductsAsync();
         Task<Product> GetByIdAsync(int? id);
         Task<Product> GetProductCategoryAsync(int? id);
-        Task<Product> CreateAsync(Category category);
-        Task<Product> UpdateAsync(Category category);
-        Task<Product> RemoveAsync(Category category);
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> RemoveAsync(Product product);
     }
 }
