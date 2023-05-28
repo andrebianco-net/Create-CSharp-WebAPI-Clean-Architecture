1801b81b19a54bd2883aba622dedbeb9de324c11
andrebianco-net <abianco.allegro@gmail.com>
2023-05-28 18:42:13 -0300

    Updating DependencyInjection class with references toServices and AutoMapper. Adding package to project too.

--- ProductRegistrationService.Infra.IoC/DependencyInjection.cs
+++ ProductRegistrationService.Infra.IoC/DependencyInjection.cs
@@ -1,6 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ProductRegistrationService.Application.interfaces;
+using ProductRegistrationService.Application.Mappings;
+using ProductRegistrationService.Application.Services;
 using ProductRegistrationService.Context;
 using ProductRegistrationService.Domain.interfaces;
 using ProductRegistrationService.Infra.Data.Repositories;
@@ -12,15 +15,20 @@ namespace ProductRegistrationService.Infra.IoC
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<ApplicationDbContext>(
-                options => 
+                options =>
                 options.UseSqlServer(
-                            configuration.GetConnectionString("DefaultConnection"), 
+                            configuration.GetConnectionString("DefaultConnection"),
                             b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)
                         )
             );
 
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
+
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
             return services;
         }
